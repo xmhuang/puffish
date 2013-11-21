b326def9ff11ae65981bd37792535c527a6593b8
Xueming <xueming_huang@hotmail.com>
2013-11-21 16:42:36 -0600

    captify

---
+++ config/deploy.rb
@@ -0,0 +1,48 @@
+set :application, 'puffish'
+set :repo_url, 'https://github.com/xmhuang/puffish.git'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/var/www/my_app'
+set :scm, :git
+set :branch, "master"
+set :user, ""
+set :scm_passphrase, ""
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+
+# set :format, :pretty
+# set :log_level, :debug
+
+# prompt password
+set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
